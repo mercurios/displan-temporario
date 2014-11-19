ad5f615e8e9a07f00b281b6bc83e7691d6179f66
Tassiano Alencar <tassianomac@me.com>
2014-11-19 19:04:12 -0300

    Iniciando as funções de adicionar e remover coluna das tabelas

---
+++ intranet/app/views/home/index.php
@@ -0,0 +1,59 @@
+<div class="container-fluid">
+    <h1 class="ls-title-intro">Planos</h1>
+
+    <h2 class="ls-title-3">
+        <button id="addColuna" class="ls-btn-default ls-ico-plus">Adicionar coluna</button>
+        <button id="rmvColuna" class="ls-btn-danger ls-ico-plus">Remover coluna</button>
+    </h2>
+
+    <hr>
+
+    <table class="ls-table ls-table-striped ls-bg-header">
+        <tr class="titulo-tabelas">
+            <td width="100">Idades</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="Título"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+        <tr class="valor-tabelas">
+            <td width="100">00-18</td>
+            <td><input class="col-md-10" type="text" name="" placeholder="150,00"></td>
+        </tr>
+    </table>
+
+
+</div>

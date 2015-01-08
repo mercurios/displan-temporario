398028eb051b3f4c704a7b7f00a8a4abf0664b7e
mercurios-hially <hially@mercurios.in>
2015-01-08 15:35:18 -0300

    Div Expansível e correções JavaScript

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,7 +5,7 @@
                 </div>
                 <div class="content col-md-10">
                     <div class="border-right inside col-md-4">
-                        <h5 class="titulo-medio">Planos individuais</h5>
+                        <h2 class="titulo-medio">Planos individuais</h2>
                         <img class="col-md-11 no-padding" src="<?= base_url('assets/img/individuais.jpg'); ?>" alt="individuais" title="Planos Individuais">
                         <div class="operadoras">
                             <ul class="col-md-12">
@@ -25,38 +25,44 @@
                         </div>
                     </div>
                     <div class="border-right inside col-md-4">
-                        <h5 class="titulo-medio">Planos empresriais</h5>
+                        <h2 class="titulo-medio">Planos empresriais</h2>
                         <img class="col-md-11 no-padding" src="<?= base_url('assets/img/empresas.png'); ?>" alt="empresas" title="Planos Individuais">
-                        <div class="operadoras">
-                            <ul class="col-md-12">
-                                <?php
-                                foreach ($operadoras as $o) :
-                                    $cat = unserialize($o->categoria);
-                                    if (in_array('empresariais', $cat)) :
-                                ?>
-                                    <li>
-                                        <a href="<?= base_url('planos-empresariais/' . $o->slug) ?>">
-                                            <?= $this->images->thumb('operadoras/' . $o->logo, 130, 50, '', 'operadora-img'); ?>
-                                            <h5><?= $o->name ?></h5>
-                                        </a>
-                                    </li>
-                                <?php endif; endforeach; ?>
-                            </ul>
+                        <div class="operadoras full">
+                            <div class="lista mini" estado="fechado">
+                                <ul class="col-md-12">
+                                    <?php
+                                    foreach ($operadoras as $o) :
+                                        $cat = unserialize($o->categoria);
+                                        if (in_array('empresariais', $cat)) :
+                                    ?>
+                                        <li>
+                                            <a href="<?= base_url('planos-empresariais/' . $o->slug) ?>">
+                                                <?= $this->images->thumb('operadoras/' . $o->logo, 130, 50, '', 'operadora-img'); ?>
+                                                <h5><?= $o->name ?></h5>
+                                            </a>
+                                        </li>
+                                    <?php endif; endforeach; ?>
+                                </ul>
+                            </div>
+                            <div class="btnAbrir" id="btnAbrir"></div>
                         </div>
                     </div>
                     <div class="inside col-md-4">
-                        <h5 class="titulo-medio">Planos para profissionais</h5>
+                        <h2 class="titulo-medio">Planos para profissionais</h2>
                         <img class="col-md-11 no-padding" src="<?= base_url('assets/img/profissionais.png'); ?>" alt="profissionais" title="Planos Individuais">
-                        <div class="operadoras">
-                            <ul class="col-md-12">
-                                <?php foreach ($categorias as $c) : ?>
-                                    <li>
-                                        <a href="<?= base_url('planos-especiais/' . $c->slug) ?>">
-                                            <h5><?= $c->name ?></h5>
-                                        </a>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
+                        <div class="operadoras full">
+                            <div class="lista mini" estado="fechado">
+                                <ul class="col-md-12">
+                                    <?php foreach ($categorias as $c) : ?>
+                                        <li>
+                                            <a href="<?= base_url('planos-especiais/' . $c->slug) ?>">
+                                                <h5><?= $c->name ?></h5>
+                                            </a>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </div>
+                            <div class="btnAbrir" id="btnAbrir2"></div>
                         </div>
                     </div>
                 </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
     // Slider
     $('.carousel').carousel({
-        interval: 1000;
+		interval: 800
     })
+	var $f = jQuery.noConflict();
+
+     // Use jQuery via $f(...)
+     $f(document).ready(function(){
+
+        $f('.btnAbrir').bind('click', function(){
+            if ($f(this).parents(".full").children(".mini").attr('estado') == 'fechado'){
+                $f(this).parents(".full").children(".mini").addClass('listaAberta');
+                $f(this).parents(".full").children(".btnAbrir").addClass('btnFechar')
+
+                $f(this).parents(".full").children(".mini").attr('estado','aberto');
+            }
+            else
+            {
+                $f(this).parents(".full").children(".mini").removeClass('lista listaAberta').addClass('lista mini');
+
+                $f(this).parents(".full").children(".btnAbrir").removeClass('btnFechar');
+
+                $f(this).parents(".full").children(".mini").attr('estado', 'fechado');
+            }
+        })
+	});
 });

b483da1bf8c384d6ba5536a8b30deed2fc29ecd2
Tassiano Alencar <tassianomac@me.com>
2015-04-22 17:29:50 -0300

    Refatoração do controller Home

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,32 +1,38 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-
+class Home extends CI_Controller
+{
+    /**
+     * ----------------------------------------
+     * Carregamento dos models
+     * ----------------------------------------
+     */
     public function __construct()
     {
         parent::__construct();
-
+        
         $this->load->model('Operadora_model', 'operadoras');
         $this->load->model('Categoria_model', 'categorias');
         $this->load->model('Tabelas_individual', 'tbind');
     }
-
+    
+    /**
+     * ----------------------------------------
+     * Exibe a página home do site
+     * ----------------------------------------
+     */
     public function index()
     {
         $metaTags = [
-            "title"         => "Displan Saúde - Home",
-            "description"   => "Displan Seguros. Planos diferenciados para empresas, profissionais e individuais.",
-            "keywords"      => "Displan, Seguros, Preços"
+            'title'         => 'Displan Saúde - Home',
+            'description'   => 'Displan Seguros. Planos diferenciados para empresas, profissionais e individuais.',
+            'keywords'      => 'Displan, Seguros, Preços'
         ];
-
-        $dados = array(
+        
+        $dados = [
             'operadoras' => $this->operadoras->all(),
             'categorias' => $this->categorias->all(),
             'metaTags'   => $metaTags
-        );
-
-        $this->load->view('inc/header', $dados);
-        $this->load->view('home/index', $dados);
-        $this->load->view('inc/footer');
-    }
+        ];
+    }   
 }
